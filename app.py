697f1696fa70a6abcf94c5a4f11c6c9f5e2c2458
Julian Lees <julian.j.lees@gmail.com>
2017-11-04 15:45:07 +1300

    Customer by username query route added 04/11/2017

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, url_for, json, Response
 from mysql_db import MySQL_Database
+import os
 
 app = Flask(__name__)
+
+# Configure application database differently if the application is being run locally
+if os.environ['ENV_TYPE'] == 'LOCAL':
+    import configparser
+
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, 'config.ini')
+    config = configparser.ConfigParser()
+    config.read(filename)
+
+    local = config['LOCAL']
+
 
 # API root
 @app.route('/')
@@ -9,10 +22,10 @@ def api_root():
     data = {'welcome': 'xTreme Burger API'}
     return Response(json.dumps(data))
 
+
 # Customer database access
 @app.route('/customer/<username>', methods=["GET"])
 def customer_by_username(username):
-
     # Setup database connection
     db = MySQL_Database()
 
@@ -24,7 +37,8 @@ def customer_by_username(username):
     if len(customer_information) > 0:
         return Response(json.dumps(customer_information))
     else:
-        return Response(json.dumps({"customer" : "void"}))
+        return Response(json.dumps({"customer": "void"}))
+
 
 if __name__ == '__main__':
     app.run()
